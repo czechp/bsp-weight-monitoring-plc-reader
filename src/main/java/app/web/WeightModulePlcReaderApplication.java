a48d2c05e14a79c2b3dfb50422b146953570bdc8
czechp <webcoderc@gmail.com>
2022-09-30 17:22:48 +0200

    Init commit

---
+++ src/main/java/app/web/WeightModulePlcReaderApplication.java
@@ -0,0 +1,13 @@
+package app.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightModulePlcReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightModulePlcReaderApplication.class, args);
+	}
+
+}
