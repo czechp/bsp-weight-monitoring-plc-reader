822f5733d20652390b37faa174812c4bc9aa4ba5
czechp <webcoderc@gmail.com>
2022-10-18 09:16:51 +0200

    Start application in docker container in production mode

--- docker/image/init.sh
+++ docker/image/init.sh
@@ -3,4 +3,4 @@ GIT_REPO=https://github.com/czechp/bsp-weight-monitoring-plc-reader.git
 git clone $GIT_REPO /JavaProject
 cd JavaProject
 mvn clean install package -DskipTests
-java -jar /JavaProject/target/weightMonitoringPlcReader.jar  --spring.profiles.active=development
+java -jar /JavaProject/target/weightMonitoringPlcReader.jar  --spring.profiles.active=production
