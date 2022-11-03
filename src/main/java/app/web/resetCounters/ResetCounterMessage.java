0990ad7b8b7d033b2ceb313cc0f5c04f10cb767e
czechp <webcoderc@gmail.com>
2022-11-03 12:05:51 +0100

    POJO modelled on rabbitMq message

---
+++ src/main/java/app/web/resetCounters/ResetCounterMessage.java
@@ -0,0 +1,16 @@
+package app.web.resetCounters;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetCounterMessage {
+    private LocalDateTime date;
+    private List<Long> lineIds;
+}
